d5c01a33cfbf204a49b4d548211f21abc2ec7f0d
Nokwazi761 <nokwazig761@gmail.com>
2024-05-26 13:36:40 +0000

    added slide show images in the home page

--- crmApp/static/js/index.js
+++ crmApp/static/js/index.js
@@ -21,3 +21,17 @@ function onClick(element) {
   function w3_close() {
       mySidebar.style.display = "none";
   }
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+      let slides = document.getElementsByClassName("slide");
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+  
