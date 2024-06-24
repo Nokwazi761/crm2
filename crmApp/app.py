98bcd0cae47e5e1f0760af56a883065456f78641
Nokwazi761 <nokwazig761@gmail.com>
2024-06-24 11:23:00 +0000

    Pending changes exported from your codespace

--- crmApp/app.py
+++ crmApp/app.py
@@ -1,10 +1,7 @@
 # Imports
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-import app
-from flask import flash, redirect, render_template, request, url_for
 from secrets import token_hex
-from flask import Flask, render_template, redirect, url_for
 
 # SetUp
 app = Flask(__name__)
@@ -12,17 +9,13 @@ app.config['SECRET_KEY'] = token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///User.db'
 db = SQLAlchemy(app)
 
-# vars and constants
-is_authenticated = False
-
 # Models
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     surname = db.Column(db.String(20), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     gender = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(20), nullable=False)
 
@@ -32,27 +25,23 @@ class User(db.Model):
         self.phone_number = phone_number
         self.email = email
         self.gender = gender
-        self.password =  password
+        self.password = password
 
-
-
-# Code
+# Routes
 @app.route('/contact')
 def contact():
-    
     return render_template('contact.html')
 
 @app.route('/about')
 def about():
-    
     return render_template('about.html')
-
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def home():
     return render_template('index.html')
+
 @app.route('/dashboard')
 def dashboard():
     return redirect(url_for('calendar'))
@@ -73,27 +62,22 @@ def register():
         user = User(name=name, surname=surname, phone_number=phone_number, email=email, password=password, gender=gender)
         db.session.add(user)
         db.session.commit()
-        flash('user added successful')
+        flash('User added successfully')
         return redirect(url_for('login'))
-    return render_template('register.html')  
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    global is_authenticated
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email).first()
-        if user:
-            if user.password ==  password:
-                is_authenticated = True
-                return redirect(url_for('home'))
-            else:
-                flash('Login Unsuccessful. Please check username and password')
+        if user and user.password == password:
+            return redirect(url_for('home'))  # Redirect to home page on successful login
         else:
             flash('Login Unsuccessful. Please check username and password')
     return render_template('login.html')
-  
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
